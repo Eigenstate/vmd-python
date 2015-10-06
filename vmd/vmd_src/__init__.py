a553c62ffeded5b18598676c36ac7e6db72092e0
Robin Betz <robin@robinbetz.com>
2015-10-05 17:38:43 -0700

    Set LD_LIBRARY_PATH before importing vmd.so so that it can find the libraries in the conda installation for which it was linked against

--- vmd/vmd_src/__init__.py
+++ vmd/vmd_src/__init__.py
@@ -23,6 +23,12 @@ os.environ['VMDDIR'] = resource_filename(__name__, "vmd.so").replace("/vmd.so","
 # Need VMD python scripts accessible (for import molecule, etc)
 sys.path.append(resource_filename(__name__, "scripts/python/VMD.py").replace("VMD.py", ""))
 
+# Need to put python installation libs in LD_LIBRARY_PATH since that is what
+# vmd.so was dynamically linked to at compile time
+# This is only during the duration of this executable
+libdir = os.path.abspath(sys.executable.replace("/bin/python","/lib"))
+os.environ['LD_LIBRARY_PATH'] = "%s:%s" % (libdir, os.environ['LD_LIBRARY_PATH'])
+
 # Load the library
 imp.load_dynamic(__name__, resource_filename(__name__, "vmd.so"))
 
