3450d32258ebbde8d8ca3ac0f7bd52768078432e
Robin Betz <robin@robinbetz.com>
2017-07-12 15:13:15 -0700

    Make setup.py much more robust to locations of files and directories.
    Now the required libraries and includes are searched for first in the
    same directories as the python installation and then in the system
    search paths. This should fix issue #4.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 from distutils.util import convert_path
 from distutils.command.build import build as DistutilsBuild
 from distutils.cmd import Command
-from subprocess import check_call
+from subprocess import check_call, check_output
 from glob import glob
 import platform
 import os
@@ -55,31 +55,127 @@ class VMDBuild(DistutilsBuild):
 
     #==========================================================================
 
-    def set_environment_variables(self, pydir):
-        os.environ["LD_LIBRARY_PATH"] = "%s:%s" % (os.path.join(pydir, "lib"),
-                                                   os.environ.get("LD_LIBRARY_PATH", ""))
-        os.environ["NETCDFLIB"] = "-L%s" % os.path.join(pydir, "lib")
-        os.environ["NETCDFINC"] = "-I%s" % os.path.join(pydir, "include")
+    def _find_include_dir(self, incfile, pydir):
+        """
+        Finds the path containing an include file. Starts by searching
+        $INCLUDE, then whatever system include paths gcc looks in.
+        If it can't find the file, defaults to "$pydir/include"
+        """
+
+        # Look in directories specified by $INCLUDE
+        searchdirs = [d for d in os.environ.get("INCLUDE", "").split(":")
+                      if os.path.isdir(d)]
+        # Also look in the directories gcc does
+        try:
+            out = check_output("echo | gcc -E -Wp,-v - 2>&1 | grep '^\s.*'",
+                               shell=True)
+            out = out.decode("utf-8").strip().split("\n")
+            searchdirs.extend([d.strip() for d in out if os.path.isdir(d.strip())])
+        except: pass
+
+        # Find the actual file
+        out = b""
+        try:
+            out = check_output(["find", "-H"]
+                                + searchdirs
+                                + ["-maxdepth", "1",
+                                   "-readable",
+                                   "-name", incfile],
+                               close_fds=True,
+                               stderr=open(os.devnull, 'wb'))
+        except: pass
+
+        incdir = os.path.split(out.decode("utf-8").split("\n")[0])[0]
+        if not glob(os.path.join(incdir, incfile)): # Glob allows fildcards
+            incdir = os.path.join(pydir, "include", incfile)
+            print("\nWARNING: Could not find include file '%s' in standard "
+                  "include directories.\n Defaulting to: '%s'"
+                  % (incfile, incdir))
+        return incdir
+
+    #==========================================================================
+
+    def _find_library_dir(self, libfile, pydir):
+        """
+        Finds the directory containing a library file. Starts by searching
+        $LD_LIBRARY_PATH, then ld.so.conf system paths used by gcc
+        """
+
+        # Look in directories specified by $LD_LIBRARY_PATH
+        out = b""
+        searchdirs = [d for d in os.environ.get("LD_LIBRARY_PATH",
+                                                "").split(":")
+                      if os.path.isdir(d)]
+        try:
+            out = check_output(["find", "-H"]
+                               + searchdirs
+                               + ["-maxdepth", "1",
+                                  "-readable",
+                                  "-name", libfile],
+                               close_fds=True,
+                               stderr=open(os.devnull, 'wb'))
+        except: pass
+        libdir = os.path.split(out.decode("utf-8").split("\n")[0])[0]
+        if glob(os.path.join(libdir, libfile)): # Glob allows wildcards
+            return libdir
+
+        # See if the linker can find it, alternatively
+        try:
+            out = check_output("ldconfig -p | grep libnetcdf.so$", shell=True)
+        except: pass
+        libdir = os.path.split(out.decode("utf-8").split(" ")[-1])[0]
+
+        if not glob(os.path.join(libdir, libfile)):
+            libdir = os.path.join(pydir, "lib", libfile)
+            print("WARNING: Could not find library file '%s' in standard "
+                  "library directories.\n Defaulting to: '%s'"
+                  % (libfile, libdir))
+        return libdir
+
+    #==========================================================================
+
+    def _find_netcdf(self, pydir):
+
+        libdir = self._find_library_dir("libnetcdf.settings", pydir)
+        incdir = self._find_include_dir("netcdf.h", pydir)
 
         # Get netcdfldflags from settings file
         pattern = re.compile(r"Extra libraries:\s(.*)")
-        with open(os.path.join(pydir, "lib", "libnetcdf.settings")) as fn:
+        with open(os.path.join(libdir, "libnetcdf.settings")) as fn:
             lines = "\n".join(fn.readlines())
             matches = re.findall(pattern, lines)
         os.environ["NETCDFLDFLAGS"] = "-lnetcdf %s" % matches[0] if len(matches) else ""
+
+        # Set appropriate environment variables
+        os.environ["NETCDFLIB"] = "-L%s" % os.path.join(libdir)
+        os.environ["NETCDFINC"] = "-I%s" % os.path.join(incdir)
         os.environ["NETCDFDYNAMIC"] = "1"
 
-        os.environ["TCLLIB"] = "-L%s" % os.path.join(pydir, "lib")
-        os.environ["TCLINC"] = "-I%s" % os.path.join(pydir, "include")
+    #==========================================================================
+
+    def set_environment_variables(self, pydir):
+        os.environ["LD_LIBRARY_PATH"] = "%s:%s" % (os.path.join(pydir, "lib"),
+                                                   os.environ.get("LD_LIBRARY_PATH", ""))
+        os.environ["INCLUDE"] = "%s:%s"  % (os.path.join(pydir, "include"),
+                                            os.environ.get("INCLUDE", ""))
+
+        # Yes, all of these redundant variables are used in the build...
+        os.environ["TCL_LIBRARY_DIR"] = self._find_library_dir("libtcl.so", pydir)
+        os.environ["TCL_INCLUDE_DIR"] = self._find_include_dir("tcl.h", pydir)
+        os.environ["TCLLIB"] = "-L%s" % os.environ["TCL_LIBRARY_DIR"]
+        os.environ["TCLINC"] = "-I%s" % os.environ["TCL_INCLUDE_DIR"]
         os.environ["TCLLDFLAGS"] = "-ltcl"
 
-        os.environ["SQLITELIB"] = "-L%s" % os.path.join(pydir, "lib")
-        os.environ["SQLITEINC"] = "-I%s" % os.path.join(pydir, "include")
+        os.environ["SQLITELIB"] = "-L%s" % self._find_library_dir("libsqlite3.so", pydir)
+        os.environ["SQLITEINC"] = "-I%s" % self._find_include_dir("sqlite3.h", pydir)
         os.environ["SQLITELDFLAGS"] = "-lsqlite3"
 
-        os.environ["EXPATLIB"] = "-L%s" % os.path.join(pydir, "lib")
-        os.environ["EXPATINC"] = "-I%s" % os.path.join(pydir, "include")
+        os.environ["EXPATLIB"] = "-L%s" % self._find_library_dir("libexpat.so", pydir)
+        os.environ["EXPATINC"] = "-I%s" % self._find_include_dir("expat.h", pydir)
         os.environ["EXPATLDFLAGS"] = "-lexpat"
+
+        # Netcdf is more complicated so it has its own method
+        self._find_netcdf(pydir)
 
         # Ask numpy where it is
         import numpy
@@ -87,14 +183,13 @@ class VMDBuild(DistutilsBuild):
         os.environ["NUMPY_LIBRARY_DIR"] = numpy.get_include().replace("include","lib")
 
         from distutils import sysconfig
-        os.environ["TCL_LIBRARY_DIR"] = "%s" % os.path.join(pydir, "lib")
-        os.environ["TCL_INCLUDE_DIR"] = "%s" % os.path.join(pydir, "include")
         os.environ["PYTHON_LIBRARY_DIR"] = sysconfig.get_python_lib()
         os.environ["PYTHON_INCLUDE_DIR"] = sysconfig.get_python_inc()
 
-        # Get python linker flag
-        libs = glob(os.path.join(pydir, "lib",
-                                 "libpython%s*.so" % sysconfig.get_python_version()))
+        # Get python linker flag, as it may be -l3.6m or -l3.6 depending on malloc
+        # this python was built against
+        pylibname = "libpython%s*.so" % sysconfig.get_python_version()
+        libs = glob(os.path.join(self._find_library_dir(pylibname, pydir), pylibname))
         libs = sorted(libs, key=lambda x: len(x))
         pythonldflag = os.path.split(libs[-1])[-1].replace("lib", "-l").replace(".so", "")
         os.environ["VMDEXTRALIBS"] = " ".join([os.environ["SQLITELDFLAGS"],
