5236fad28e2e743961333380b18711226f23917a
Robin Betz <robin@robinbetz.com>
2018-02-27 14:29:40 -0800

    Move __init__.py into vmd_src so build directory doesn't look like an importable python package. Fixes test failures.

--- vmd/install.sh
+++ vmd/install.sh
@@ -54,7 +54,7 @@ make clean
 rm $vmd_src/vmd_src/plugins
 
 # Copy init.py file into build directory
-cp "$vmd_src/__init__.py" "$VMDDIR"
+cp "$vmd_src/vmd_src/__init__.py" "$VMDDIR"
 
 # Copy tests into build directory so they're accessible
 cp -r "$vmd_src/../test" "$VMDDIR"
