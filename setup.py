bc00966827188ed62e1bc0770ef4596ffe60cb9e
Robin Betz <robin@robinbetz.com>
2018-03-11 19:42:05 -0700

    Bump version: 2.0.9 → 2.0.10

--- setup.py
+++ setup.py
@@ -348,7 +348,7 @@ class VMDTest(Command):
 ###############################################################################
 
 setup(name='vmd-python',
-      version='2.0.9',
+      version='2.0.10',
       description='Visual Molecular Dynamics Python module',
       author='Robin Betz',
       author_email='robin@robinbetz.com',
--- vmd/vmd_src/__init__.py
+++ vmd/vmd_src/__init__.py
@@ -9,7 +9,7 @@ Official VMD web page: http://www.ks.uiuc.edu/Research/vmd/
 
 """
 
-__version__ = '2.0.9'
+__version__ = '2.0.10'
 __author__ = 'Robin Betz'
 
 import imp
