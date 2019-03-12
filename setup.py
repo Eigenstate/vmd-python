34169ac1d0f4dea4552bb73f7a96d849493f10aa
Robin Betz <robin@robinbetz.com>
2019-03-12 12:30:06 -0700

    Bump version: 3.0.1 → 3.0.2

--- setup.py
+++ setup.py
@@ -394,7 +394,7 @@ class VMDTest(Command):
 ###############################################################################
 
 setup(name='vmd-python',
-      version='3.0.1',
+      version='3.0.2',
       description='Visual Molecular Dynamics Python module',
       author='Robin Betz',
       author_email='robin@robinbetz.com',
