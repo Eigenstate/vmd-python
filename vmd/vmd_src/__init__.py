83c90128542aac8bbb049a0e909d10db9520da4d
Robin Betz <robin@robinbetz.com>
2025-01-27 21:00:19 -0800

    Make names consistent in init

--- vmd/vmd_src/__init__.py
+++ vmd/vmd_src/__init__.py
@@ -20,29 +20,29 @@ import importlib.util
 import importlib.machinery
 
 # Need to set VMDDIR environment variable for vmd.so to import
-vmdlib = importlib.resources.files(__name__) / "libvmd.so"
-os.environ["VMDDIR"] = os.path.split(vmdlib)[0]
+vmd = importlib.resources.files(__name__) / "libvmd.so"
+os.environ["VMDDIR"] = os.path.split(vmd)[0]
 os.environ["VMDDISPLAYDEVICE"] = "OPENGLPBUFFER"  # For off screen rendering
 
 # Load the library
 
-loader = importlib.machinery.ExtensionFileLoader(__name__, str(vmdlib))
+loader = importlib.machinery.ExtensionFileLoader(__name__, str(vmd))
 spec = importlib.util.spec_from_loader(__name__, loader)
-vmdlib = importlib.util.module_from_spec(spec)
-loader.exec_module(vmdlib)
+vmd = importlib.util.module_from_spec(spec)
+loader.exec_module(vmd)
 
 # These modules define classes for convenient manipulation of VMD state.
 # Import them strangely so they're in the same python namespace
-vmdlib.Molecule = importlib.machinery.SourceFileLoader(
+vmd.Molecule = importlib.machinery.SourceFileLoader(
     "Molecule", os.path.join(os.environ["VMDDIR"], "Molecule.py")
 ).load_module()
-vmdlib.Label = importlib.machinery.SourceFileLoader("Label", os.path.join(os.environ["VMDDIR"], "Label.py")).load_module()
-vmdlib.Material = importlib.machinery.SourceFileLoader(
+vmd.Label = importlib.machinery.SourceFileLoader("Label", os.path.join(os.environ["VMDDIR"], "Label.py")).load_module()
+vmd.Material = importlib.machinery.SourceFileLoader(
     "Material", os.path.join(os.environ["VMDDIR"], "Material.py")
 ).load_module()
 
 # Set version and author
-vmdlib.__version__ = __version__
-vmdlib.__author__ = __author__
+vmd.__version__ = __version__
+vmd.__author__ = __author__
 
-sys.modules["vmd"] = vmdlib
+sys.modules["vmd"] = vmd
